b8c5e6750c651e95e6a083adfa75956522330a2c
Reyer Swengel <reyerswengel@gmail.com>
2019-01-26 10:37:44 -0600

    Player controller prototyping

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+
+    public LayerMask clickableLayer;
+
+    public EventVector3 OnClickEnvironment;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Clickable"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    OnClickEnvironment.Invoke(hit.point);
+                }
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
