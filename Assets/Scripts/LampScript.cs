9b4469d9fc69d94013da245ebf73c07d6419549d
Reyer Swengel <reyerswengel@gmail.com>
2019-01-26 20:28:35 -0600

    blah

---
+++ Assets/Scripts/LampScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampScript : MonoBehaviour
+{
+    Light light;
+    bool increase = true;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        light.intensity = (Random.value / 10) + 0.5f;
+    }
+
+    void Update()
+    {
+        if(increase)
+        {
+            light.intensity += 0.01f;
+            if (light.intensity > 0.6f) increase = false;
+        }
+        else
+        {
+            light.intensity -= 0.01f;
+            if (light.intensity < 0.5f) increase = true;
+        }
+    }
+}
