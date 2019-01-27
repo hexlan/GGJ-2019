de8c163134f5149e48d32c3c0c2c871fcae41419
Reyer Swengel <reyerswengel@gmail.com>
2019-01-27 06:39:13 -0600

    Lots of work on level 2

---
+++ Assets/Scripts/WispFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispFollow : MonoBehaviour
+{
+    public EventVector3 OnClickEnvironment;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        var heading = player.transform.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+
+        if (distance > 8)
+        {
+            OnClickEnvironment.Invoke(transform.position + direction * 7);
+        }
+    }
+}
