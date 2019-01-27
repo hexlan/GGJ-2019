e4d78f0fc43761551b9fb287846f5f53bcf9cc69
Reyer Swengel <reyerswengel@gmail.com>
2019-01-27 11:32:09 -0600

    Stuff once again

---
+++ Assets/Scripts/LevelThreePlayer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelThreePlayer : MonoBehaviour
+{
+    public DialogManager dialog;
+    public MouseManager mouse;
+
+    int state = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Dialog_1")
+        {
+            var lookPos = GameObject.FindGameObjectWithTag("Wisp").transform.position - transform.position;
+            lookPos.y = 0;
+            transform.rotation = Quaternion.LookRotation(lookPos);
+            Destroy(other.gameObject);
+
+            GameObject.FindGameObjectWithTag("Wisp").GetComponent<WispFollow>().enabled = false;
+
+            DialogSnippet[] snippet =
+            {
+                new DialogSnippet("Wisp", "Last time to talk before you go in there...")
+            };
+
+            dialog.StartDialog(snippet);
+        }
+        else if (other.gameObject.tag == "End")
+        {
+            var lookPos = GameObject.FindGameObjectWithTag("Wisp").transform.position - transform.position;
+            lookPos.y = 0;
+            transform.rotation = Quaternion.LookRotation(lookPos);
+
+
+            DialogSnippet[] snippet =
+            {
+                new DialogSnippet("Boy", "I is winner...")
+            };
+
+            dialog.StartDialog(snippet);
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (!GetComponent<NavMeshAgent>().pathPending)
+        {
+            if (GetComponent<NavMeshAgent>().remainingDistance <= GetComponent<NavMeshAgent>().stoppingDistance)
+            {
+                if (!GetComponent<NavMeshAgent>().hasPath || GetComponent<NavMeshAgent>().velocity.sqrMagnitude == 0f)
+                {
+                    if (mouse.mushroom)
+                    {
+                        mushroom();
+                    }
+                    else if (mouse.lantern)
+                    {
+                        GameObject.FindGameObjectWithTag("Lantern").SetActive(false);
+                        state = 1;
+                        mouse.lantern = false;
+
+                        var lookPos = GameObject.FindGameObjectWithTag("Wisp").transform.position - transform.position;
+                        lookPos.y = 0;
+                        transform.rotation = Quaternion.LookRotation(lookPos);
+
+                        DialogSnippet[] snippets = new DialogSnippet[] { new DialogSnippet("Wisp", "You found the lantern...") };
+                        dialog.StartDialog(snippets);
+                    }
+                }
+            }
+        }
+    }
+    
+    private void mushroom()
+    {
+        var lookPos = GameObject.FindGameObjectWithTag("Mushroom").transform.position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
+
+        DialogSnippet[] snippets = new DialogSnippet[] { new DialogSnippet("Mushroom", "This dialog, should not happen...") };
+        switch (state)
+        {
+            case 0:
+                snippets = new DialogSnippet[]{
+                    new DialogSnippet("Mushroom", "No heading this way without the lantern of light.")
+                };
+                break;
+            case 1:
+                snippets = new DialogSnippet[]{
+                    new DialogSnippet("Mushroom", "You found the lantern, go ahead.")
+                };
+
+                GameObject.FindGameObjectWithTag("PathBlocker").SetActive(false);
+                break;
+        }
+
+        dialog.StartDialog(snippets);
+
+        mouse.mushroom = false;
+    }
+}
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -14,6 +14,8 @@ public class MouseManager : MonoBehaviour
     public bool hag = false;
     public bool eyeball = false;
     public bool frog = false;
+    public bool mushroom = false;
+    public bool lantern = false;
 
     // Update is called once per frame
     void Update()
@@ -24,6 +26,18 @@ public class MouseManager : MonoBehaviour
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50))
             {
+                if (hit.collider.gameObject.tag == "Lantern")
+                {
+                    hit.collider.gameObject.transform.Find("Light").GetComponent<Light>().intensity = 3;
+                }
+                else
+                {
+                    var lantern = GameObject.FindGameObjectWithTag("Lantern");
+                    if (lantern != null) {
+                        lantern.transform.Find("Light").GetComponent<Light>().intensity = 0.5f;
+                    }
+                }
+
                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Clickable"))
                 {
                     if (Input.GetMouseButtonDown(0))
@@ -47,14 +61,26 @@ public class MouseManager : MonoBehaviour
                             frog = true;
                             OnClickEnvironment.Invoke(new Vector3(8f, 12.631f, 9));
                         }
+                        else if (hit.collider.gameObject.tag == "Mushroom")
+                        {
+                            mushroom = true;
+                            OnClickEnvironment.Invoke(new Vector3(7, 0, -12));
+                        }
+                        else if (hit.collider.gameObject.tag == "Lantern")
+                        {
+                            lantern = true;
+                            OnClickEnvironment.Invoke(new Vector3(8, 0, 16));
+                        }
                         else
                         {
                             wisp = false;
                             hag = false;
                             eyeball = false;
                             frog = false;
+                            mushroom = false;
+                            lantern = false;
                             OnClickEnvironment.Invoke(hit.point);
-                            
+
                         }
                     }
                 }
