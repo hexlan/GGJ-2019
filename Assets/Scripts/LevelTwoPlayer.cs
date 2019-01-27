6c51af4826a9531656011c466b8cc643d213d922
Reyer Swengel <reyerswengel@gmail.com>
2019-01-27 04:31:20 -0600

    Messed some stuff up, making save point

---
+++ Assets/Scripts/LevelTwoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTwoPlayer : MonoBehaviour
+{
+    public DialogManager dialog;
+    public MouseManager mouse;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
